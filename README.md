ca068f9fa5d83a7f01ae4fc46731f441807e5c03
MalyarchukNA <MalyarchukNikita@yandex.ru>
2026-05-01 18:04:50 +0300

    feat: add README.md, modify pom.xml
    - Added README.md
    - Added properties to pom.xml
    - Created GameServlet.java with empty doPost method

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # interactive-web-quest
-Web-based text quest built with Java Servlets, JSP and Maven. 
+
+Web-based text quest built with Java Servlets, JSP and Maven.
+
+## *1. Стек технологий*
+
+```text
+1. Java 21
+2. Maven 
+3. Servlet API 4.0
+```
+
+## *2. Функционал*
+
+```text
+Приложение веб-квест, которое принимает решения пользователя, хранит состояния игры, показывает результат.
+```
+
+## *3. Инструкция по запуску*
+
+```text
+Требует Java 21 и Maven 3.9
+```
+
--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -5,11 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Стартовый сервлет, перенаправляет на главную страницу index.jsp
+ */
+
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("index.jsp").forward(req, resp);
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
